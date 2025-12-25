3574d467942e1f11eeba591d77023e2746649c3f
NikDS <NikerinDS@gmail.com>
2025-12-25 18:30:55 +0500

    Исправлены замечания (Изменен порядок методов)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -20,6 +20,35 @@ public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
     private int nextId = 0;
 
+    @GetMapping
+    public ResponseEntity<Collection<Film>> getAllFilms() {
+        log.info("Запрос на получения списка фильмов");
+        return new ResponseEntity<>(films.values(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Film> createFilm(@RequestBody Film film) {
+        log.info("Запрос на создание фильма:{}", film);
+        validate(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Создан фильм:{}", film);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Film> updateFilm(@RequestBody Film film) {
+        log.info("Запрос на обновление фильма:{}", film);
+        if (!films.containsKey(film.getId())) {
+            log.warn("Не найден фильм с id:{}", film.getId());
+            throw new NotFoundException("Не найден фильм с id=" + film.getId());
+        }
+        validate(film);
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм:{}", film);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
     private int getNextId() {
         return ++nextId;
     }
@@ -42,34 +71,5 @@ public class FilmController {
             log.warn("Ошибка валидации: duration <= 0 ");
             throw new ValidationException("Продолжительность фильма должна быть положительным числом");
         }
-    }
-
-    @GetMapping
-    public ResponseEntity<Collection<Film>> getAllFilms() {
-        log.info("Запрос на получения списка фильмов");
-        return new ResponseEntity<>(films.values(), HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Film> createFilm(@RequestBody Film film) {
-        log.info("Запрос на создание фильма:{}",film);
-        validate(film);
-        film.setId(getNextId());
-        films.put(film.getId(), film);
-        log.info("Создан фильм:{}",film);
-        return new ResponseEntity<>(film, HttpStatus.OK);
-    }
-
-    @PutMapping
-    public ResponseEntity<Film> updateFilm(@RequestBody Film film) {
-        log.info("Запрос на обновление фильма:{}",film);
-        if (!films.containsKey(film.getId())) {
-            log.warn("Не найден фильм с id:{}",film.getId());
-            throw new NotFoundException("Не найден фильм с id=" + film.getId());
-        }
-        validate(film);
-        films.put(film.getId(), film);
-        log.info("Обновлен фильм:{}",film);
-        return new ResponseEntity<>(film, HttpStatus.OK);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -20,29 +20,6 @@ public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
     private int nextId = 0;
 
-    private int getNextId() {
-        return ++nextId;
-    }
-
-    private void validate(User user) {
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            log.warn("Ошибка валидации: login пустой или содержит пробелы");
-            throw new ValidationException("login пользователя не может быть пустым или содержать пробелы");
-        }
-        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
-            log.warn("Ошибка валидации: birthDate не указана или позже текущей даты");
-            throw new ValidationException("Дата рождения пользователя должна быть указана и не быть в будущем");
-        }
-        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            log.warn("Ошибка валидации: email некорректен");
-            throw new ValidationException("Email пользователя должен быть корректным");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.info("пустое поле name перезаписано значением login");
-            user.setName(user.getLogin());
-        }
-    }
-
     @GetMapping
     public ResponseEntity<Collection<User>> getAllUsers() {
         log.info("Запрос на получения списка пользователей");
@@ -70,5 +47,28 @@ public class UserController {
         users.put(user.getId(), user);
         log.info("Обновлен пользователь:{}", user);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    private int getNextId() {
+        return ++nextId;
+    }
+
+    private void validate(User user) {
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.warn("Ошибка валидации: login пустой или содержит пробелы");
+            throw new ValidationException("login пользователя не может быть пустым или содержать пробелы");
+        }
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Ошибка валидации: birthDate не указана или позже текущей даты");
+            throw new ValidationException("Дата рождения пользователя должна быть указана и не быть в будущем");
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Ошибка валидации: email некорректен");
+            throw new ValidationException("Email пользователя должен быть корректным");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.info("пустое поле name перезаписано значением login");
+            user.setName(user.getLogin());
+        }
     }
 }
